5c5f4e9acfdcb9b2a53b9f8d3c3efc78a8694543
hugocharon <charon.hugo@hotmail.com>
2025-04-10 17:04:58 +0200

    [Angular2] #Config - Changement de la variable de production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     API_URLS: {
-        GET_JOBS: 'http://localhost:4200/jobs',
+        GET_JOBS: 'https://jobsearch-angular-2.vercel.app/jobs',
     },
 };
